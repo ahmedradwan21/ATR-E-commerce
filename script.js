a2ffd9f68fafe9ac8fe2e6708e672933ad2445e5
Ahmed Tarek Radwan (ATR) <100035760+ahmedradwan21@users.noreply.github.com>
2023-05-18 14:08:04 +0300

    hello ATR

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// const bar = document.getElementById("bar");
-// const close = document.getElementById("close");
-// const nav = document.getElementById("navbar");
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
 
-// const carticon = document.querySelector("#lg-bag");
-// const cart = document.getElementById("navbar");
-// const closecart = document.getElementById("navbar");
+const carticon = document.querySelector("#lg-bag");
+const cart = document.getElementById("navbar");
+const closecart = document.getElementById("navbar");
 
 const container = document.getElementById("container");
 const signInEmail = document.getElementById("signInEmail");
@@ -13,35 +13,35 @@ const signUpEmail = document.getElementById("signUpEmail");
 // opensingnIN
 function openSignIn() {
 	container.classList.remove("right-panel-active");
-	// if (signUpEmail.value !== "") {
-	// 	signInEmail.value = signUpEmail.value;
-	// }
+	if (signUpEmail.value !== "") {
+		signInEmail.value = signUpEmail.value;
+	}
 }
 // opensingnUp
 function openSignUp() {
 	container.classList.add("right-panel-active");
-	// if (signInEmail.value !== "") {
-	// 	// signUpEmail.value = signInEmail.value;
-	// }
+	if (signInEmail.value !== "") {
+		// signUpEmail.value = signInEmail.value;
+	}
 }
 
 
 
 
-// const observer = new IntersectionObserver((entries) => {
-// 	entries.forEach((entry)=>{
-// 		console.log(entry)
-// 		if(entry.isIntersecting){
-// 			entry.target.classList.add('show');
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry)=>{
+		console.log(entry)
+		if(entry.isIntersecting){
+			entry.target.classList.add('show');
 
-// 		}else{
-// 			entry.target.classList.remove('show')
-// 		}
-// 	})
-// })
-// const hiddenElements = document.querySelectorAll('.hidden');
+		}else{
+			entry.target.classList.remove('show')
+		}
+	})
+})
+const hiddenElements = document.querySelectorAll('.hidden');
 
-// hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((el) => observer.observe(el));
 
 
 //    start menu
@@ -62,13 +62,13 @@ const openMenu = () => {
 		menuDOM.style.transition = ".5s";
 	}, 10);
 	
-	// allSecTitle.forEach((st) => {
-	// 	st.style.zIndex = "0";
-	// });
-	// try {
-	// 	prog.style.zIndex = "0";
-	// } catch (error) {}
-	// document.lastChild.style.overflow = "hidden"; // Disables the window scrolling
+	allSecTitle.forEach((st) => {
+		st.style.zIndex = "0";
+	});
+	try {
+		prog.style.zIndex = "0";
+	} catch (error) {}
+	document.lastChild.style.overflow = "hidden"; // Disables the window scrolling
 };
 
 // CLOSE MENU
@@ -78,30 +78,30 @@ const closeMenu = () => {
 	menuDOM.style.transition = ".5s";
 	setTimeout(() => {
 		menuDOM.style.display = "none";
-		// allSecTitle.forEach((st) => {
-		// 	st.style.zIndex = "";
-		// });
-		// try {
-		// 	prog.style.zIndex = "";
-		// } catch (error) {}
+		allSecTitle.forEach((st) => {
+			st.style.zIndex = "";
+		});
+		try {
+			prog.style.zIndex = "";
+		} catch (error) {}
 	}, 500);
-	// document.lastChild.style.overflow = ""; // ReEnables the window scrolling
+	document.lastChild.style.overflow = ""; // ReEnables the window scrolling
 };
 
-// CLOSES MENU IF ANY AREA OUTSIDE THE MENU BOX GETS CLICKED
-// window.addEventListener("click", (e) => {
-// 	if (menuDOM.style.display === "block") {
-// 		let parent =
-// 		e.target.parentNode.parentNode.parentNode.parentNode === menuDOM ||
-// 		e.target.parentNode.parentNode.parentNode === menuDOM ||
-// 			e.target.parentNode.parentNode === menuDOM ||
-// 			e.target.parentNode === menuDOM;
+CLOSES MENU IF ANY AREA OUTSIDE THE MENU BOX GETS CLICKED
+window.addEventListener("click", (e) => {
+	if (menuDOM.style.display === "block") {
+		let parent =
+		e.target.parentNode.parentNode.parentNode.parentNode === menuDOM ||
+		e.target.parentNode.parentNode.parentNode === menuDOM ||
+			e.target.parentNode.parentNode === menuDOM ||
+			e.target.parentNode === menuDOM;
 			
-// 		if (!parent && e.target !== menuBtn) {
-// 			closeMenu();
-// 		}
-// 	}
-// });
+		if (!parent && e.target !== menuBtn) {
+			closeMenu();
+		}
+	}
+});
 
 
 // //  finsh menu
